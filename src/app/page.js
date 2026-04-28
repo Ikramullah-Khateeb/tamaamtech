8d03d97d103060c0ee519def0a3e4a2ff27d4060
afeef-khan <afeefkhan95413@gmail.com>
2026-04-28 14:57:36 +0530

    Our services section

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+// import Hero from "@/components/Hero";
+import OurServices from "@/components/OurServices";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Heading</h1>
+      <OurServices/>
+      <p>paragraph</p>
+    </div>
+  );
+}
